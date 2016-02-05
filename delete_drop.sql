f90e755013132f8217358463b50646b6c9e2060b
Tobin Mori <ttmori@pbs.org>
2016-02-05 15:55:13 -0500

    add delete sql

---
+++ delete_drop.sql
@@ -0,0 +1,57 @@
+delete from orgentity_showentity_map;
+delete from orgentity_image_map;
+delete from orgentity_link_map;
+
+delete from orgentity_distributor;
+delete from orgentity_producer;
+delete from orgentity_station;
+delete from orgentity_audience;
+delete from orgentity;
+delete from object_aggregate;
+delete from series_parent_child_map;
+delete from series_otos_map;
+
+
+delete from series_child;
+delete from series_ep_season;
+delete from season;
+delete from series_parent;
+delete from franchise;
+
+delete from episode_series;
+delete from episode_oto;
+delete from episode_base;
+delete from showentity_image_map;
+delete from image;
+delete from showentity_link_map;
+delete from link;
+delete from series_base;
+delete from showentity;
+
+drop table orgentity_audience;
+drop table orgentity_distributor;
+drop table orgentity_producer;
+drop table orgentity_station;
+drop table orgentity_showentity_map;
+drop table orgentity_image_map;
+drop table orgentity_link_map;
+drop table orgentity;
+drop table series_parent_child_map;
+drop table series_child;
+drop table series_ep_season;
+drop table series_otos_map;
+drop table series_parent;
+drop table season;
+
+
+drop table object_aggregate;
+drop table franchise;
+drop table episode_oto;
+drop table episode_series;
+drop table episode_base;
+drop table showentity_image_map;
+drop table showentity_link_map;
+drop table image;
+drop table link;
+drop table series_base;
+drop table showentity;
